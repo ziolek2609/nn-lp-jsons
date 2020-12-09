6597d12b85028ce35f39c545a229c57da829b97c
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2020-12-09 19:35:15 +0100

    Add files via upload

--- jsons.py
+++ jsons.py
@@ -1,7 +1,13 @@
+import json
+
 # podsumowuje parametry sieci neronowej
 def nn_params(source):
-    with open(source, "r") as data:
-        neural_network = json.load(data)
+    if source == str:
+        with open(source, "r") as data:
+            neural_network = json.load(data)
+    else:
+        neural_network = source
+
     nn_params = {"atoms":{"sum": 0, "inp": 0, "hid": 0, "out": 0, "rec": 0}, "connections":{"inp2Hid": 0, "hid2Out": 0, "rec": 0}, "bigWeights": {"inp2Hid": 0, "hid2Out": 0, "rec": 0}, "factors":{}}
     nn_params["atoms"]["sum"] = len((neural_network)["nn"]["inpLayer"])+len((neural_network)["nn"]["hidLayer"])+len((neural_network)["nn"]["outLayer"])
     nn_params["atoms"]["inp"] = len((neural_network)["nn"]["inpLayer"]) # atomy na inputLayer
@@ -25,28 +31,31 @@ def nn_params(source):
     nn_params["factors"] = neural_network["nnFactors"]
     return json.dumps(nn_params)
 
-
 # podsumowuje parametry programu logicznego
 def lp_params(source):
-    with open(source, "r") as data:
-        logic_program = json.load(data)
+    if source == str:
+        with open(source, "r") as data:
+            logic_program = json.load(data)
+    else:
+        logic_program = source
+
     lp_params = {"clauses": {"amount": 0, "onlyPos": 0, "onlyNeg": 0, "mix": 0, "headWithH": 0, "atoms": {"sum": 0, "pos": 0, "neg": 0, "withH": 0, "posWithH": 0, "negWithH": 0}, "difAtoms": {"sum": 0, "pos": 0, "neg": 0, "withH": 0, "posWithH": 0, "negWithH": 0}}, "facts": 0, "assumptions": 0}
     atoms = []
     atoms_pos = []
     atoms_neg = []
-    for i in range(len(logic_program)):
-        if logic_program[i]["tag"] == "Cl": # klauzule
+    for i in range(len(logic_program["lp"]["clauses"])):
+        if logic_program["lp"]["clauses"][i]["tag"] == "Cl": # klauzule
             lp_params["clauses"]["amount"] += 1
-            if len(logic_program[i]["clPAtoms"]) == 0: # klauzule onlyNegAtoms
+            if len(logic_program["lp"]["clauses"][i]["clPAtoms"]) == 0: # klauzule onlyNegAtoms
                 lp_params["clauses"]["onlyNeg"] +=1
-            elif len(logic_program[i]["clNAtoms"]) == 0: # klauzule onlyPosAtoms
+            elif len(logic_program["lp"]["clauses"][i]["clNAtoms"]) == 0: # klauzule onlyPosAtoms
                 lp_params["clauses"]["onlyPos"] +=1
             else: #klauzule mixAtoms
                 lp_params["clauses"]["mix"] +=1
-            lp_params["clauses"]["atoms"]["sum"] += len(logic_program[i]["clPAtoms"]) + len(logic_program[i]["clNAtoms"]) # suma klauzul
-            if logic_program[i]["clHead"]["label"] == "h": # klauzule z hHead
+            lp_params["clauses"]["atoms"]["sum"] += len(logic_program["lp"]["clauses"][i]["clPAtoms"]) + len(logic_program["lp"]["clauses"][i]["clNAtoms"]) # suma klauzul
+            if logic_program["lp"]["clauses"][i]["clHead"]["label"] == "h": # klauzule z hHead
                 lp_params["clauses"]["headWithH"] += 1
-            for j in logic_program[i]["clPAtoms"]: # atomy pozytywne
+            for j in logic_program["lp"]["clauses"][i]["clPAtoms"]: # atomy pozytywne
                 lp_params["clauses"]["atoms"]["pos"] +=1
                 if j["label"] == "h": # atomy pozytywne z h
                         lp_params["clauses"]["atoms"]["withH"] += 1
@@ -59,7 +68,7 @@ def lp_params(source):
                     if j["label"] == "h": #atomy pozytywne bez powtórzeń z h
                         lp_params["clauses"]["difAtoms"]["withH"] += 1
                         lp_params["clauses"]["difAtoms"]["posWithH"] += 1
-            for j in logic_program[i]["clNAtoms"]: # atomy negatywne
+            for j in logic_program["lp"]["clauses"][i]["clNAtoms"]: # atomy negatywne
                 lp_params["clauses"]["atoms"]["neg"] +=1
                 if j["label"] == "h": # atomy negatywne z h
                         lp_params["clauses"]["atoms"]["withH"] += 1
@@ -73,8 +82,7 @@ def lp_params(source):
                         lp_params["clauses"]["difAtoms"]["withH"] += 1
                         lp_params["clauses"]["difAtoms"]["negWithH"] += 1
             lp_params["clauses"]["difAtoms"]["sum"] = len(atoms) # atomy bez powtórzeń
-        elif logic_program[i]["tag"] == "Fact": # fakty
-            lp_params["facts"] += 1
-        elif logic_program[i]["tag"] == "Assumption": # assumpitons
-            lp_params["assumptions"] += 1
+
+    lp_params["facts"] += len(logic_program["lp"]["facts"]) # fakty
+    lp_params["assumptions"] += len(logic_program["lp"]["assumptions"]) # assumptions
     return json.dumps(lp_params)
